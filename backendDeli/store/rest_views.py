b8e9c65175b8e41fcfe5ad8014722d7309528600
asingjr2 <asingjr2@gmail.com>
2018-07-05 23:50:46 -0400

    adding updates to readme

--- backendDeli/store/rest_views.py
+++ backendDeli/store/rest_views.py
@@ -8,7 +8,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import list_route
 
-from drf_multiple_model.mixins import FlatMultipleModelMixin, ObjectMultipleModelMixin
+
 
 from rest_framework.generics import GenericAPIView
 
